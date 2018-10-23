3661f034e54d027b3c0e083b9799c45d04b45aee
piyush78 <peyushchowhan7@gmail.com>
2018-10-23 19:30:21 +0530

    changed database to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_17_150613) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ideas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_150613) do
     t.string "Tags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_indices_on_user_id"
   end
 
@@ -44,4 +47,5 @@ ActiveRecord::Schema.define(version: 2018_10_17_150613) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "indices", "users"
 end
